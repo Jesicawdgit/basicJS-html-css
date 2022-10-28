aed4688654b640e2652697a63445151e87d5e6d6
Jesica <jesicamichel2004j@gmail.com>
2022-10-28 14:07:40 +0530

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+var h1 = document.querySelector("h1")
+var body = document.querySelector("body")
+
+
+function changeText(){
+    // h1.textContent = "Aashiwaka"
+    body.classList.add("bc")
+}
+function alert(){
+    body.alert("jessica modda")
+}
